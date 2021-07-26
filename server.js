dbf71800bed80bb3d7f11728fc539179de71d2da
‘samahAbujwaied’ <samah.abujweed@yahoo.com>
2021-07-26 20:57:13 +0300

    add

--- server.js
+++ server.js
@@ -8,18 +8,6 @@ app.use(express.json());
 const port=process.env.PORT || 8080
 const {getDegURL,postDegURL,getDegFavURL,deleteDegFavURL,updateDegFavURL} = require('./controller/user.controller')
 const mongoose = require('mongoose');
-// mongoose
-//   .connect("mongodb://localhost:27017/movies", {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => {
-//     console.log("Connected to the database!");
-//   })
-//   .catch((err) => {
-//     console.log("Cannot connect to the database!", err);
-//     process.exit();
-//   });
 mongoose.connect(
 'mongodb://samah:1234@degmond-shard-00-00.kchnw.mongodb.net:27017,degmond-shard-00-01.kchnw.mongodb.net:27017,degmond-shard-00-02.kchnw.mongodb.net:27017/degmond?ssl=true&replicaSet=atlas-105kwd-shard-0&authSource=admin&retryWrites=true&w=majority',
   // 'mongodb://localhost:27017/degmond',  
@@ -35,8 +23,8 @@ app.post('/postDegURL',postDegURL)
 app.get('/getDegFavURL',getDegFavURL)
 app.delete('/deleteDegFavURL/:id',deleteDegFavURL)
 app.put('/updateDegFavURL/:id',updateDegFavURL)
-app.get('/',()=>{
-    res.send('Is connected');
+app.get('/',(req,res)=>{
+    res.json('Is connected');
 })
 app.listen(port,()=>{
     console.log('Is connected');
