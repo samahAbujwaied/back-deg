9d73adba56f5ec6453765635d791d46372e23ae1
‘samahAbujwaied’ <samah.abujweed@yahoo.com>
2021-07-26 22:48:24 +0300

    add

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 'use strict'
+
 require('dotenv').config();
 const express = require('express');
 const app = express();
@@ -23,9 +24,14 @@ app.post('/postDegURL',postDegURL)
 app.get('/getDegFavURL',getDegFavURL)
 app.delete('/deleteDegFavURL/:id',deleteDegFavURL)
 app.put('/updateDegFavURL/:id',updateDegFavURL)
-app.get('/',(req,res)=>{
-    res.json('Is connected');
-})
+// app.get('/',(req,res)=>{
+//     res.json('Is connected');
+// })
+app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next()
+  });
 app.listen(port,()=>{
     console.log('Is connected');
 })
